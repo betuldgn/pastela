d527f1e399eccd4072e9e2cf19558f91b5e85c65
betuldgn <bbetuldogann@gmail.com>
2026-02-21 20:37:08 +0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      'https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell',
+      'https://router.huggingface.co/models/black-forest-labs/FLUX.1-schnell',
       {
         method: 'POST',
         headers: {
